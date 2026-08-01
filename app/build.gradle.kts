b6dd4dfe1eeef548cbefd46529dd89b013effacd
baxiy56177 <baxiy56177@amupx.com>
2026-08-02 01:39:09 +0300

    Recreate app/build.gradle.kts with JavaMail dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // JavaMail for email sending
+    implementation("com.sun.mail:android-mail:1.6.7")
+    implementation("com.sun.mail:android-activation:1.6.7")
 }
