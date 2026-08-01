ee20403033c4eaff5b8cb394617f3bd62e2605fa
baxiy56177 <baxiy56177@amupx.com>
2026-08-02 00:05:08 +0300

    Add GitHub Actions workflow

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
