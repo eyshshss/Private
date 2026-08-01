eef6250a5d2d87a2ca5b97df4a1238b87c1bab24
baxiy56177 <baxiy56177@amupx.com>
2026-08-02 01:14:12 +0300

    Recreate build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,4 @@
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
